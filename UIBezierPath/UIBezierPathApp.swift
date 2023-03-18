2458b97b6eb66fb1b921a131bf448ac36f471894
Eason <Eason9630@gmail.com>
2023-03-18 23:48:44 +0800

    Initial Commit

---
+++ UIBezierPath/UIBezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBezierPathApp.swift
+//  UIBezierPath
+//
+//  Created by 林祔利 on 2023/3/18.
+//
+
+import SwiftUI
+
+@main
+struct UIBezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
